21f090435bdde9b4f7af0fd9a26ea1740ca3d3e2
Hao Xiangpeng <3150102255@zju.edu.cn>
2016-03-31 18:05:32 +0800

    Finished a basic to-do list

--- lists/tests.py
+++ lists/tests.py
@@ -3,7 +3,47 @@ from django.core.urlresolvers import resolve
 from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from lists.models import Item
 # Create your tests here.
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retriving_items(self):
+        first_item=Item.objects.create(text='The first (ever) list item')
+        second_item=Item.objects.create(text='The second list item')
+
+        saved_items=Item.objects.all()
+        self.assertEqual(saved_items.count(),2)
+
+        first_saved_item=saved_items[0]
+        second_saved_item=saved_items[1]
+        self.assertEqual(first_saved_item.text,'The first (ever) list item')
+        self.assertEqual(second_saved_item.text,'The second list item')
+    def test_home_page_can_save_a_Post_request(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+
+        response=home_page(request)
+
+        self.assertEqual(Item.objects.count(),1)
+        new_item=Item.objects.first()
+        self.assertEqual(new_item.text,'A new list item')
+
+        #self.assertIn('A new list item',response.content.decode())
+        excepted_html=render_to_string(
+                'home.html',
+                {'new_item_text': 'A new list item'})
+
+    def test_home_page_can_redirect(self):
+        request=HttpRequest()
+        request.method='POST'
+        request.POST['item_text']='A new list item'
+
+        response=home_page(request)
+
+        self.assertEqual(response.status_code,302)
+        self.assertEqual(response['location'],'/')
 
 class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
@@ -21,10 +61,18 @@ class HomePageTest(TestCase):
         request.POST['item_text']='A new list item'
 
         response=home_page(request)
-        self.assertIn('A new list item',response.content.decode())
+        #self.assertIn('A new list item',response.content.decode())
         excepted_html=render_to_string(
                 'home.html',
                 {'new_item_text':'A new list item'}
                 )
-        self.assertEqual(response.content.decode(),excepted_html)
+    def test_home_page_can_list_all_items(self):
+        Item.objects.create(text='first list')
+        Item.objects.create(text='Second list')
+
+        request=HttpRequest()
+        response=home_page(request)
+
+        self.assertIn('first list',response.content.decode())
+        self.assertIn('Second list',response.content.decode())
 
--- lists/views.py
+++ lists/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.http import HttpResponse
-
+from lists.models import Item
 # Create your views here.
 
 def home_page(request):
     if request.method=='POST':
-        return render(request,'home.html',{'new_item_text':request.POST.get('item_text','')})
-    return render(request,'home.html')
+        item_text=request.POST['item_text']
+        Item.objects.create(text=item_text)
+        return redirect('/')
+    else:
+        item_text=Item.objects.all()
+    return render(request,'home.html',{'new_item_text':item_text})
+    #return render(request,'home.html')
