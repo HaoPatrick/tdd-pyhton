82fda8a85db6bd80b151f3e056d2054c333d2880
Hao Xiangpeng <3150102255@zju.edu.cn>
2016-04-02 11:07:06 +0800

    Refactor the url pattern, making it more elegant

--- superlists/urls.py
+++ superlists/urls.py
@@ -13,14 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url,include
 from django.contrib import admin
 import lists.views
 urlpatterns = [
     #url(r'^admin/', admin.site.urls),
     url(r'^$',lists.views.home_page,name='/'),
     #url(r'^list/the-only-list-in-the-world/$',lists.views.view_list,name='view_list'),
-    url(r'^list/(\d+)/$',lists.views.view_list,name='view_list'),
-    url(r'^list/(\d+)/add_item$',lists.views.add_item,name='add_item'),
-    url(r'^list/new$',lists.views.new_list,name='new_list'),
+    url(r'^list/',include('lists.urls')),
 ]
